1fec90d716d39a29902e1581cedcb4e245746252
shubhamsrstvpersonal <shubham.srstv@gmail.com>
2020-04-09 14:04:17 +0530

    Changes in rest endpoint service

--- src/main/java/com/springbatch/services/BatchServices.java
+++ src/main/java/com/springbatch/services/BatchServices.java
@@ -2,7 +2,10 @@ package com.springbatch.services;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
+import com.springbatch.exception.PortalException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,24 +21,61 @@ import com.springbatch.utility.BatchUtil;
 public class BatchServices {
 
 	private static final Logger LOGGER = LogManager.getLogger(BatchServices.class);
-	
+
+	@Value("${status.message.initiated}")
+    private String initiated;
+
+    @Value("${status.message.success}")
+    private String success;
+
+    @Value("${status.message.fail}")
+    private String fail;
+
     @Autowired
     DaoClass daoClass;
     
     @Value("${esb.retry.attempt.limit}")
     private Long attemptLimit;
 
-    public KycSaveDataResponseBean saveData(String kycData, String cif) throws Exception{
-
-        String formattedDate = BatchUtil.getDateFormat(new Date());
+    public KycSaveDataResponseBean saveOrUpdateKYCData(String kycData, Map<String, Object> headers) throws PortalException{
 
         KycCustomerData kycCustomerData = new KycCustomerData();
-        kycCustomerData.setId(cif+formattedDate);
+
+        String cif = headers.get("cif").toString();
+        Object refNo = headers.get("kycrefno");
+
+        if (refNo != null) {
+            String id = refNo.toString();
+            Optional<KycCustomerData> data = daoClass.findById(id);
+            if (data.isPresent()) {
+                LOGGER.debug("Updating the existing data.");
+                KycCustomerData customerData = data.get();
+
+                kycCustomerData.setId(id);
+                if (headers.get("status").toString().equals("0")) {
+                    kycCustomerData.setStatus(success);
+                } else {
+                    kycCustomerData.setStatus(fail);
+                }
+                kycCustomerData.setCount(customerData.getCount() + 1);
+                kycCustomerData.setCreatedOn(customerData.getCreatedOn());
+                kycCustomerData.setUpdatedOn(new Date());
+            }
+            else {
+                throw new PortalException("KYC102");
+            }
+        }
+        else {
+            LOGGER.debug("Creating a new entry.");
+            String formattedDate = BatchUtil.getDateFormat(new Date());
+            kycCustomerData.setId(cif+formattedDate);
+            kycCustomerData.setStatus(initiated);
+            kycCustomerData.setCount(1);
+            kycCustomerData.setCreatedOn(new Date());
+        }
+
         kycCustomerData.setKycData(kycData);
         kycCustomerData.setCif(cif);
-        kycCustomerData.setCount(1);
-        kycCustomerData.setStatus("Initiated");
-        kycCustomerData.setCreatedOn(new Date());
 
         daoClass.save(kycCustomerData);
 
@@ -47,6 +87,6 @@ public class BatchServices {
     }
 
     public List<KycCustomerData> getData() throws Exception{
-    	return daoClass.findByStatusAndCountLessThan("Failed", attemptLimit);
+    	return daoClass.findByStatusAndCountLessThan(fail, attemptLimit);
     }
 }
